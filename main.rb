3873c7e0e8abe73f225b3103fd12c80eaadfbfc4
Arpith Siromoney <arpith@feedreader.co>
2018-04-13 21:46:17 -0400

    initial code

---
+++ main.rb
@@ -0,0 +1,25 @@
+require 'socket'
+require 'base64'
+NTP_FIELDS = [ :byte1, :stratum, :poll, :precision, :delay, :delay_fb,
+                   :disp, :disp_fb, :ident, :ref_time, :ref_time_fb, :org_time,
+                   :org_time_fb, :recv_time, :recv_time_fb, :trans_time,
+                   :trans_time_fb ]
+sock = UDPSocket.new
+sock.connect("pool.ntp.org", 123)
+bytes = Array.new(48, 0)
+bytes[0] = 0x1b
+msg = bytes.pack("c*")
+sock.print(msg)
+sock.flush
+read, write, error = IO.select [sock], nil, nil
+if read.nil?
+  p "help"
+else
+  data, _ = sock.recvfrom(960)
+  packetdata = data.unpack("a C3   n B16 n B16 H8   N B32 N B32   N B32 N B32")
+  packet_data_by_field = {}
+  NTP_FIELDS.each do |field|
+    packet_data_by_field[field] = packetdata.shift
+  end
+  p packet_data_by_field 
+end
